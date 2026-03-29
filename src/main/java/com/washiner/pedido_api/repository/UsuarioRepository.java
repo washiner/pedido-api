c8e235810f0477f7fc941a6406b239af33ee01d2
Washiner <washiner@gmail.com>
2026-03-30 08:43:07 +0900

    adicionei o UsuarioRepository

---
+++ src/main/java/com/washiner/pedido_api/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.washiner.pedido_api.repository;
+
+import com.washiner.pedido_api.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
