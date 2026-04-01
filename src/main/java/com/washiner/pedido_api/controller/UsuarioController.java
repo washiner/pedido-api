1f5a18b716abc3047b4fa0a155994a7c3a5da042
Washiner <washiner@gmail.com>
2026-04-01 22:24:05 +0900

    feat: adicionei UsuarioController com endpoints básicos

---
+++ src/main/java/com/washiner/pedido_api/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package com.washiner.pedido_api.controller;
+
+import com.washiner.pedido_api.entity.Usuario;
+import com.washiner.pedido_api.service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public Usuario criar(@RequestBody Usuario usuario) {
+        return usuarioService.salvar(usuario);
+    }
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return usuarioService.listar();
+    }
+}
