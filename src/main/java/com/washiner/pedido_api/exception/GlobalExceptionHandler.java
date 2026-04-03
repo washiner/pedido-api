7fbbfa209bc41be88e9d03db36061ef27edc36d6
Washiner <washiner@gmail.com>
2026-04-03 21:45:07 +0900

    feat: adiciona tratamento global de exceções

---
+++ src/main/java/com/washiner/pedido_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.washiner.pedido_api.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
